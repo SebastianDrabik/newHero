799f9dfc03f6e984fc48b5a6063db6f34d3504da
SemiGodProgrammer <kacpik07@gmail.com>
2022-12-28 16:20:29 +0100

    Added player animations & main menu, fixed gaps between tiles bug

---
+++ New Hero/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
--- New Hero/Assets/Scripts/PlayerMovement.cs
+++ New Hero/Assets/Scripts/PlayerMovement.cs
@@ -6,14 +6,47 @@ public class PlayerMovement : MonoBehaviour
     private float horizontal;
     public int speed=2;
 
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
     // Update is called once per frame
     void Update()
     {
         vertical = Input.GetAxisRaw("Vertical");
         horizontal = Input.GetAxisRaw("Horizontal");
         
-        vertical *= Time.deltaTime;
-        horizontal *= Time.deltaTime;
+        if(vertical!=0 || horizontal != 0)
+        {
+            animator.SetBool("Moving", true);
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+        }
+
+        if (animator.GetFloat("Horizontal") == 0)
+        {
+            animator.SetFloat("Vertical", vertical);
+        }
+
+        if (animator.GetFloat("Vertical") == 0)
+        {
+            animator.SetFloat("Horizontal", Mathf.Abs(horizontal));
+        }
+
+
+        if (horizontal < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        if (horizontal > 0 || (horizontal==0 && vertical!=0))
+        {
+            spriteRenderer.flipX = false;
+        }
+
+        //vertical *= Time.deltaTime;
+        //horizontal *= Time.deltaTime;
 
         //transform.Translate(horizontal * speed, vertical * speed, 0);
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(horizontal * speed, vertical * speed);
