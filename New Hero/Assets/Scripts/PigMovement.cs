d3c81cf5a1aa94017b7776e593a0b662f01ca074
SemiGodProgrammer <kacpik07@gmail.com>
2022-12-30 15:24:31 +0100

    Updated map

---
+++ New Hero/Assets/Scripts/PigMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigMovement : MonoBehaviour
+{
+    Vector2 speed = new Vector2();
+    private void Start()
+    {
+        speed = new Vector2(Random.Range(-0.8f, .8f), Random.Range(-1f, 1f));
+        gameObject.GetComponent<SpriteRenderer>().flipX = speed.x > 0 ? true : false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.GetComponent<Rigidbody2D>().velocity = speed;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Fence")
+        {
+            speed.y *= -1f;
+        }
+        else if(collision.gameObject.tag == "House")
+        {
+            speed.x *= -1f;
+        }
+        gameObject.GetComponent<SpriteRenderer>().flipX = speed.x > 0 ? true : false;
+
+    }
+}
