e55ff55812a3e585fcb9d01df48a9481482d924b
VirozPL <virozgames@gmail.com>
2023-01-13 21:18:42 +0100

    Added template functions
    Added template functions for battle system and fixed battle system UI

--- New Hero/Assets/Scripts/FightManager.cs
+++ New Hero/Assets/Scripts/FightManager.cs
@@ -1,11 +1,41 @@
-using System.Collections.Generic;
-using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro;
 
 public class FightManager : MonoBehaviour
 {
+    public FightManager instance { get; set; }
+    public void Awake()
+    {
+        if(instance == null)
+            instance = this;
+    }
+
     [Space]
-    public GameObject attack;
-    public List<string> code;
+    [Header("Top text gameobject")]
+    public TextMeshProUGUI top;
+    [Header("Bottom text gameobject")]
+    public TextMeshProUGUI bottom;
+    [Space]
+    [Header("Bottom text gameobject")]
+    public TMP_InputField codeInput;
+    private string correctCode;
+
+    public void OpenCodeEditor(string top, string bottom, string correctCode)
+    {
+        this.correctCode = correctCode;
+        this.top.text = top;
+        this.bottom.text = bottom;
+        gameObject.SetActive(true);
+    }
+
+    public void RunCode()
+    {
+        Debug.Log(this.codeInput.text.Trim() == this.correctCode);
+//        return this.codeInput.text.Trim() == this.correctCode;
+    }
+
+    public void CloseCodeEditor()
+    {
+        gameObject.SetActive(false);
+    }
 }
