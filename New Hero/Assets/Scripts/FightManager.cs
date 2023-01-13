ffe99ca04953c004ef3b932a7b414b5353355fac
VirozPL <virozgames@gmail.com>
2023-01-13 19:29:03 +0100

    Added basic UI for battle system

---
+++ New Hero/Assets/Scripts/FightManager.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FightManager : MonoBehaviour
+{
+    [Space]
+    public GameObject attack;
+    public List<string> code;
+}
