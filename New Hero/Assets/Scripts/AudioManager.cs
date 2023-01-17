534f8dddbd17635f70f3b9d768436ca22880b1db
SemiGodProgrammer <kacpik07@gmail.com>
2023-01-17 10:46:48 +0100

    Fixed Audio Manager(I hope)

--- New Hero/Assets/Scripts/AudioManager.cs
+++ New Hero/Assets/Scripts/AudioManager.cs
@@ -39,31 +39,62 @@ public class AudioManager : MonoBehaviour
     void Start()
     {
         currentScene = SceneManager.GetActiveScene().name;
-        if (PlayerPrefs.HasKey("Marco_Defeated") && SceneManager.GetActiveScene().name == "MarkCube_Boss")
+        //if (PlayerPrefs.HasKey("Marco_Defeated") && SceneManager.GetActiveScene().name == "MarkCube_Boss")
+        //{
+        //    if (PlayerPrefs.GetInt("Marco_Defeated") == 1)
+        //    {
+        //        StopAll();
+        //        Play("MainTheme");
+        //    }else if(PlayerPrefs.GetInt("Marco_Defeated") == 0)
+        //    {
+        //        StopAll();
+        //        Play("Marco_Music_Loop");
+        //    }
+        //}
+        //else if (currentScene != "MarkCube_Boss")
+        //{
+        //    Play("MainTheme");
+        //}
+        //else
+        //{
+        //    Play("Marco_Music_Loop");
+        //}
+
+
+        Debug.Log(IsPlaying("MainTheme"));
+        if (currentScene != "MarkCube_Boss" && !IsPlaying("MainTheme"))
         {
-            if (PlayerPrefs.GetInt("Marco_Defeated") == 1)
-            {
-                StopAll();
-                Play("MainTheme");
-            }else if(PlayerPrefs.GetInt("Marco_Defeated") == 0)
-            {
-                StopAll();
-                Play("Marco_Music_Loop");
-            }
-        }
-        else if (currentScene != "MarkCube_Boss")
-        {
+            StopAll();
             Play("MainTheme");
+        }else if(!PlayerPrefs.HasKey("Marco_Defeated")|| PlayerPrefs.GetInt("Marco_Defeated") == 0)
+        {
+            StopAll();
+            Play("Marco_Music_Loop");
         }
         else
         {
-            Play("Marco_Music_Loop");
+            StopAll();
+            Play("MainTheme");
         }
+
     }
 
     private void Update()
     {
-        if(SceneManager.GetActiveScene().name != currentScene)
+
+        currentScene = SceneManager.GetActiveScene().name;
+        if (currentScene != "MarkCube_Boss" && !IsPlaying("MainTheme"))
+        {
+            StopAll();
+            Play("MainTheme");
+        }else if(!IsPlaying("Marco_Music_Loop") && SceneManager.GetActiveScene().name == "MarkCube_Boss" && (!PlayerPrefs.HasKey("Marco_Defeated") || PlayerPrefs.GetInt("Marco_Defeated") == 0))
+        {
+            StopAll();
+            Play("Marco_Music_Loop");
+        }
+
+
+        /*if (SceneManager.GetActiveScene().name != currentScene)
         {
             if(SceneManager.GetActiveScene().name == "MarkCube_Boss")
             {
@@ -76,7 +107,7 @@ public class AudioManager : MonoBehaviour
                 Play("MainTheme");
             }
             currentScene = SceneManager.GetActiveScene().name;
-        }
+        }*/
     }
 
 
@@ -89,6 +120,17 @@ public class AudioManager : MonoBehaviour
             return;
         }
         s.source.Play();
+    }
+
+    public bool IsPlaying(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Audio: " + name + " not found");
+            return false;
+        }
+        return s.source.isPlaying;
     }
 
     public void Stop(string name)
