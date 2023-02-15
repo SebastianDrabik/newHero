d30c1e2ea08930e205ba126ded1fcc63ac85fda5
SemiGodProgrammer <kacpik07@gmail.com>
2023-02-15 16:27:37 +0100

    Started work on lessons

--- New Hero/Assets/Scripts/Managers/LessonManager.cs
+++ New Hero/Assets/Scripts/Managers/LessonManager.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class LessonManager : MonoBehaviour
 {
     public GameObject classExit;
     private GameManager manager;
+    //[System.Serializable]
+    //struct
+
+    //private List<struct> codes;
 
     void Start()
     {
@@ -11,10 +16,15 @@ public class LessonManager : MonoBehaviour
 
         if (SaveSystem.level >= SaveData.Level.CPP_BASICS)
         {
-            gameObject.SetActive(false);
+            //gameObject.SetActive(false);
             return;
         }
         manager.ShowObjective("lesson-seat");
         classExit.SetActive(false);
+    }
+
+    public void StartLesson()
+    {
+          
     }
 }
