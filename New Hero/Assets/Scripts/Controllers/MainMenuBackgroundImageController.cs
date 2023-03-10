af56ebf7a4136d4ff5085b4de1f4b6a51e056bed
Viroz <68112314+zoriV@users.noreply.github.com>
2023-03-10 20:26:56 +0100

    Added background image transition (gh-21)

--- New Hero/Assets/Scripts/Controllers/MainMenuBackgroundImageController.cs
+++ New Hero/Assets/Scripts/Controllers/MainMenuBackgroundImageController.cs
@@ -8,6 +8,9 @@ public class MainMenuBackgroundImageController : MonoBehaviour
     public float changeTime;
     private readonly List<Sprite> images = new();
     public Image backgroundImage;
+    public Image panel;
+    public float transitionTime;
+    
     private float timer;
     private int currentImageIndex;
 
@@ -24,12 +27,35 @@ public class MainMenuBackgroundImageController : MonoBehaviour
         if(timer <= 0f)
         {
             timer = changeTime;
-            currentImageIndex++;
-            if(currentImageIndex >= images.Count)
-                currentImageIndex = 0;
-            backgroundImage.sprite = images[currentImageIndex];
+            StartCoroutine(nameof(Transition));
         }
         timer-=Time.deltaTime;
+    }
+
+
+    private IEnumerator Transition()
+    {
+        panel.gameObject.SetActive(true);
+        for (int i = 0; i < 100; i++)
+        {
+            Color color = panel.color;
+            color.a = (float)i / 100f;
+            panel.color = color;
+            yield return new WaitForSeconds(transitionTime/100);
+        }
+        
+        currentImageIndex++;
+        if (currentImageIndex >= images.Count)
+            currentImageIndex = 0;
+        backgroundImage.sprite = images[currentImageIndex];
+
+        for (int i = 100; i >= 0; i--)
+        {
+            Color color = panel.color;
+            color.a = (float)i / 100f;
+            panel.color = color;
+            yield return new WaitForSeconds(transitionTime/100);
+        }
     }
 
 }
